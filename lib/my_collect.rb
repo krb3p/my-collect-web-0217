c15c56f170898fa68dae167dc49b55ac91fd7883
krb3p <ryankatherineburke@gmail.com>
2017-02-08 18:28:50 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect (array)
+  index = 0
+  new_array = []
+  while index < array.length do
+    new_element = yield (array [index])
+    index += 1
+    new_array << new_element
+  end
+  new_array
+end
